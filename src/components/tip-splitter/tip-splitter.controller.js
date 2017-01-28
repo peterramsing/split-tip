94dd5de979ec3824a539a0514d5a67c43864fd0d
Peter Ramsing <peter.ramsing@mac.com>
2017-01-28 15:38:24 -0800

    Re-adds functionality

--- src/components/tip-splitter/tip-splitter.controller.js
+++ src/components/tip-splitter/tip-splitter.controller.js
@@ -2,9 +2,34 @@ export default class TipSplitterController {
     constructor(TipSplitterService) {
         'ngInject'
         this.tps = TipSplitterService
-        this.test = 'testing'
-        console.log('test');
+
+        this.partners = [
+            {
+                name: 'partner0',
+                hours: 0,
+            },
+            {
+                name: 'partner1',
+                hours: 0,
+            },
+        ]
+
+        this.tipTotal = 0
+
+
+
     }
+
+    calculatePartnersTips() {
+        let foo = this.tps.calculateTipSplit(this.partners, this.tipTotal)
+
+        console.log(foo.partners)
+
+        this.partners = foo.partners
+
+        // this.partners = .partners
+    }
+
 
     // $onInit() {
     //     this.test = 'test';
--- src/components/tip-splitter/tip-splitter.service.js
+++ src/components/tip-splitter/tip-splitter.service.js
@@ -2,4 +2,28 @@ export class TipSplitterService {
     doSomething() {
         alert('hi')
     }
+
+    calculateTipSplit(partners, tipTotal) {
+        let totalHours = 0
+        let totalPayOut = 0
+
+        for (var partner of partners) {
+          totalHours += partner.hours
+        }
+
+        let dollarsPerHour = tipTotal / totalHours
+
+        for (var partner of partners) {
+          partner.tips = Math.floor(partner.hours * dollarsPerHour)
+          totalPayOut += partner.tips
+        }
+
+        let remainder = tipTotal - totalPayOut
+        return {
+            remainder,
+            totalHours,
+            totalPayOut,
+            partners
+        }
+    }
 }
