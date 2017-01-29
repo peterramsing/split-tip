caf719e3367fa4cd668b1c85c8d1208037adb8a9
Peter Ramsing <peter.ramsing@mac.com>
2017-01-28 20:28:17 -0800

    I'll get these injected soon? 😟 [ci skip]

--- src/components/tip-splitter/tip-splitter.spec.js
+++ src/components/tip-splitter/tip-splitter.spec.js
@@ -1,26 +1,5 @@
 import test from 'ava';
 
-let tps = function(){}
+test.todo('Tip splitter')
 
-test.failing('Tip splitter', t => {
-
-    let partners = [
-        {
-            name: 'partner0',
-            hours: 44,
-        },
-        {
-            name: 'partner1',
-            hours: 33,
-        },
-    ]
-
-    let tipTotal = 123
-
-
-    let test = tps(partners, tipTotal)
-    let result
-
-
-    t.equal(test, result);
-});
+test.todo('Bill splitter')
