f9fc1c146cf82d2210c5ea9885bfa2b9d98258ca
Peter Ramsing <peter.ramsing@mac.com>
2017-02-11 10:21:00 -0800

    Adds trailig commas for easier diffs

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ module.exports = {
     entry: "./src/app.module.js",
     output: {
         path: __dirname,
-        filename: "./build/bundle.js"
+        filename: "./build/bundle.js",
     },
     devtool: 'source-map',
     module: {
         rules: [
             {
                 test: /\.js$/,
-                loader: 'babel-loader'
+                loader: 'babel-loader',
             },
             {
                 test: /\.html$/,
-                loader: 'html-loader'
+                loader: 'html-loader',
             },
         ]
     }
